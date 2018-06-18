bd2405a9448deb75461c955042fe6a82d780a66c
Andrew Spencer <thrawn369@gmail.com>
2018-06-18 15:35:29 +0930

    Added GameManager class to keep track of enemies

---
+++ Source/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameManager.instance.OnEnemySpawn(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDestroy()
+    {
+        GameManager.instance.OnEnemyDestroyed(this);
+    }
+}
---
+++ Source/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    List<Enemy> enemies;
+
+    public List<Enemy> Enemies { get { return enemies; } }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(this);
+
+        enemies = new List<Enemy>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnSceneChange()
+    {
+
+    }
+
+    public void OnEnemySpawn(Enemy enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    public void OnEnemyDestroyed(Enemy enemy)
+    {
+        enemies.Remove(enemy);
+        Debug.Log("Enemy destroyed");
+    }
+}
