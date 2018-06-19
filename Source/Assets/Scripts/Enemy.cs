a669e76f2e35d629dc23d326a0e39293e9f6980a
Andrew Spencer <thrawn369@gmail.com>
2018-06-19 13:12:54 +0930

    Added healthbar to bandit

--- Source/Assets/Scripts/Enemy.cs
+++ Source/Assets/Scripts/Enemy.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    List<MaterialSwapper> materialSwappers;
-
     [SerializeField]
     Canvas infoCanvas;
 
@@ -15,15 +13,27 @@ public class Enemy : MonoBehaviour {
     [SerializeField]
     float health;
 
+    [SerializeField]
+    HealthBar healthBar;
+
 	// Use this for initialization
 	void Start () {
         GameManager.instance.OnEnemySpawn(this);
         health = maxHealth;
+
+        healthBar.maxHealth = maxHealth;
+        healthBar.health = health;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         infoCanvas.transform.forward = Camera.main.transform.forward;
+
+        //HACK
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            healthBar.InflictDamage(10);
+        }
 	}
 
     private void OnDestroy()
---
+++ Source/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class HealthBar : MonoBehaviour {
+
+    public float health;
+    public float maxHealth;
+    public float damage = 0;
+    public float drainRate = 0.5f;
+
+    [SerializeField]
+    Image backgroundSprite;
+    [SerializeField]
+    Image damageSprite;
+    [SerializeField]
+    Image healthSprite;
+
+    [SerializeField]
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        backgroundSprite.fillAmount = 1;
+        damageSprite.fillAmount = 1;
+        healthSprite.fillAmount = 1;
+
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //TODO only appear after damage, then fade away
+
+        float healthProportion = health / maxHealth;
+        float damageProportion = damage / maxHealth;
+
+        anim.SetFloat("HealthProportion", healthProportion);
+        anim.SetFloat("DamageProportion", damageProportion);
+
+        healthSprite.fillAmount = healthProportion;
+        damageSprite.fillAmount = healthProportion + damageProportion;
+
+	}
+
+    public void InflictDamage(float amount)
+    {
+        damage += amount;
+        health -= amount;
+        anim.SetTrigger("Damaged");
+    }
+}
